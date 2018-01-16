2636ab9c56fb9b390ca89a0d21c61d87a60dd960
yinmotion <yinmotion.dev@gmail.com>
2018-01-16 11:15:00 -0500

    added Knobs addon

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,9 +4,13 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Button from '../components/Button';
 
-storiesOf('Components/Button/Simple', module)
-  .add('text', 
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+
+const stories = storiesOf('Components/Button', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('Text', 
   (() => 
-    <Button onClick={action('clicked')}>Button</Button>
-  )
-);
+    <Button onClick={action('clicked')} disabled={boolean('Disabled', false)}>{text('Label', 'Button')}</Button>
+  ));
